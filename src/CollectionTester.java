ec16b0c667dc91922490cc9d71e7dc74571da03f
guptaharsh25 <reyharsh25@gmail.com>
2018-09-23 17:51:54 -0400

    part 2 completed

--- src/CollectionTester.java
+++ src/CollectionTester.java
@@ -1,16 +1,45 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Vector;
 
 public class CollectionTester {
-    public static void main(String[] args){
-        long start = System.currentTimeMillis();
-        ArrayList<Integer> arrayList = CollectionTester.genRandomArrayList(319999999);
-        long end = System.currentTimeMillis();
+    static private final int CONST_SIZE = 105999999;
 
-        System.out.println(end-start);
+    public static void main(String[] args){
+        //part a
+        long start = System.currentTimeMillis();
+        ArrayList<Integer> arrayList = CollectionTester.genRandomArrayList(CONST_SIZE);
+        long end = System.currentTimeMillis();
+        System.out.println("ArrayList: " + (end-start));
+
+        start = System.currentTimeMillis();
+        int[] array = CollectionTester.genRandomArray(CONST_SIZE);
+        end = System.currentTimeMillis();
+        System.out.println("Array: " + (end-start));
+
+        start = System.currentTimeMillis();
+        Vector<Integer> vector = CollectionTester.genRandomVector(CONST_SIZE);
+        end = System.currentTimeMillis();
+        System.out.println("Vector: " + (end-start) + "\n");
+
+        //part b
+        start = System.currentTimeMillis();
+        CollectionTester.sumArrayList(arrayList);
+        end = System.currentTimeMillis();
+        System.out.println("ArrayList Iterator: " + (end-start));
+
+        start = System.currentTimeMillis();
+        CollectionTester.sumArray(array);
+        end = System.currentTimeMillis();
+        System.out.println("Array Iterator: " + (end-start));
+
+        start = System.currentTimeMillis();
+        CollectionTester.sumVector(vector);
+        end = System.currentTimeMillis();
+        System.out.println("Vector Iterator: " + (end-start));
     }
 
-    public static ArrayList<Integer> genRandomArrayList(int length){
+    private static ArrayList<Integer> genRandomArrayList(int length){
         ArrayList<Integer> arrayList = new ArrayList<>(length);
 
         for(int i = 0; i < length; i++){
@@ -20,7 +49,7 @@ public class CollectionTester {
         return arrayList;
     }
 
-    public static int[] genRandomArray(int length){
+    private static int[] genRandomArray(int length){
         int[] arr = new int[length];
         for(int i = 0; i < length; i++){
             arr[i] = (int) (Math.random()*9);
@@ -29,12 +58,38 @@ public class CollectionTester {
         return arr;
     }
 
-    public static Vector<Integer> genRandomVector(int length){
+    private static Vector<Integer> genRandomVector(int length){
         Vector<Integer> vector = new Vector<>(length);
         for(int i = 0; i < length; i++){
             vector.add((int) (Math.random()*9));
         }
 
         return vector;
+    }
+
+    private static long sumArrayList(ArrayList<Integer> arrayList){
+        Iterator<Integer> iterator = arrayList.iterator();
+        long sum = 0;
+        while(iterator.hasNext()){
+            sum += iterator.next();
+        }
+        return sum;
+    }
+
+    private static long sumArray(int[] array){
+        long sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    private static long sumVector(Vector<Integer> vector){
+        Iterator<Integer> iterator = vector.iterator();
+        long sum = 0;
+        while(iterator.hasNext()){
+            sum += iterator.next();
+        }
+        return sum;
     }
 }
